505564d60afe6c12e9bc5cb04bf23a3e0cc7b66c
Rima Deshpande <rimadeshpande@Rimas-MacBook-Air.local>
2023-03-16 23:06:45 -0500

    Added DeshpandeHelloWorld.java

---
+++ Module_1/HelloWorldApp/DeshpandeHelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        String firstName = "Rima";
+
+        String lastName = "Deshpande";
+
+        String message = "Hello World from %s %s.";
+
+        System.out.printf(message, firstName, lastName);
+
+    }
+
+}
