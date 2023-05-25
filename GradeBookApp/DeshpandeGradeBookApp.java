501ae4c3ca57038755329c7ce8c2d355e0636581
Rima Deshpande <rimadeshpande@Rimas-MacBook-Air.local>
2023-05-25 15:55:09 -0500

    GradeBookApp updated with the events Module 11

--- GradeBookApp/DeshpandeGradeBookApp.java
+++ GradeBookApp/DeshpandeGradeBookApp.java
@@ -9,8 +9,11 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.*;
+
 public class DeshpandeGradeBookApp extends Application {
-    
+    private static final String FILE_NAME = "grades.csv";
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -55,10 +58,121 @@ public class DeshpandeGradeBookApp extends Application {
         VBox layout = new VBox(10);
         layout.getChildren().addAll(formLayout, buttonsLayout);
 
+        // Create result label
+        Label resultLabel = new Label();
+
         // Create scene
-        Scene scene = new Scene(layout, 400, 300);
+        Scene scene = new Scene(new VBox(layout, resultLabel), 400, 350);
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        // Handle Save Grade button action
+        saveButton.setOnAction(event -> {
+            String firstName = firstNameTextField.getText();
+            String lastName = lastNameTextField.getText();
+            String course = courseTextField.getText();
+            String grade = gradeComboBox.getValue();
+
+            Student student = new Student(firstName, lastName, course, grade);
+            saveStudentToCSV(student);
+        });
+
+        // Handle View Grades button action
+        viewButton.setOnAction(event -> {
+            String grades = readGradesFromCSV();
+            resultLabel.setText(grades);
+        });
+    }
+
+    private void saveStudentToCSV(Student student) {
+        try (FileWriter writer = new FileWriter(FILE_NAME, true);
+             BufferedWriter bw = new BufferedWriter(writer);
+             PrintWriter pw = new PrintWriter(bw)) {
+            // Check if the file is empty, then add the header row
+            File file = new File(FILE_NAME);
+            if (file.length() == 0) {
+                pw.println("firstName,lastName,course,grade");
+            }
+            pw.println(student.getFirstName() + "," +
+                    student.getLastName() + "," +
+                    student.getCourse() + "," +
+                    student.getGrade());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readGradesFromCSV() {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    public static class Student {
+        private String firstName;
+        private String lastName;
+        private String course;
+        private String grade;
+
+        public Student() {
+            // Default constructor
+        }
+
+        public Student(String firstName, String lastName, String course, String grade) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.course = course;
+            this.grade = grade;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getCourse() {
+            return course;
+        }
+
+        public void setCourse(String course) {
+            this.course = course;
+        }
+
+        public String getGrade() {
+            return grade;
+        }
+
+        public void setGrade(String grade) {
+            this.grade = grade;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", course='" + course + '\'' +
+                    ", grade='" + grade + '\'' +
+                    '}';
+        }
     }
 }
