0b6984588132e42db716be29a68176971a2e65bc
Rima Deshpande <rimadeshpande@Rimas-MacBook-Air.local>
2023-06-02 14:44:14 -0500

    M12:GradeBookApp updated

--- GradeBookApp/DeshpandeGradeBookApp.java
+++ GradeBookApp/DeshpandeGradeBookApp.java
@@ -1,3 +1,35 @@
+/*
+ Liang, Y.D. (2019). Introduction to Java Programming and Data Structures:
+ 	Comprehensive Version (12th ed.). Pearson Education, Inc.
+ Deshpande, Rima. (2023). CIS 505 Intermediate Java Programming. Bellevue University
+
+Capstone Project Requirements
+	Name the project folder GradeBookApp.
+	Name the program file YourLastNameGradeBookApp.java.
+	Use GitHub to store the project.
+
+	Provide users with a grade book form. Include the following fields: first name, last name, course, and grade (use A-F).
+	For the grade field use a ComboBox with prefilled values.
+	Use labels to identify the forms fields.
+	Provide users with a button to clear the grade book form.
+	Provide users with a button to view saved grade entries.
+	Provide users with a button to save a grade book entry.
+
+	On form submission, write the entered values to a csv file named grades.csv.
+	The grades.csv file should have a header row with values for “firstName, lastName, course, and grade.”
+	When a user selects the “view grades’ button, display the contents of the grades.csv file.
+
+	Use a Java class to hold the values from the gradebook form (firstName, lastName, course, and grade).
+	Name the class Student and include private variables for the fields with two constructors:
+	-a default constructor and a parameterized constructor with the class’s properties.
+	-Add getter/setter methods for the class properties and Override the toString() method.
+
+	Use the Student classes toString() method to output the object to the results section of the GUI you create.
+
+	Build the Capstone Project’s user interface using JavaFX.
+	
+	add the appropriate event and error handlers to make the application fully functional.
+*/
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -82,6 +114,14 @@ public class DeshpandeGradeBookApp extends Application {
         viewButton.setOnAction(event -> {
             String grades = readGradesFromCSV();
             resultLabel.setText(grades);
+        });
+
+        // Handle Clear button action
+        clearButton.setOnAction(event -> {
+            firstNameTextField.clear();
+            lastNameTextField.clear();
+            courseTextField.clear();
+            gradeComboBox.getSelectionModel().clearSelection();
         });
     }
 
